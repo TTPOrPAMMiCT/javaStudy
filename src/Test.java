90e8e317cb0b2edd5695bc9d1da0a28d90797451
TTPOrPAMMiCT <georgeusenko@list.ru>
2020-05-30 18:44:47 +0300

    Refactor

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Test {
-    public static void main(String[] args) {
-        for (int i = 0; i <=10; i++) {
-            System.out.println(i);
-        }
-    }
-}
